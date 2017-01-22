339b67b00b10ca0fe6a1435fd13cd2d38e6958f4
Maciej Bielecki <zyla@prati.pl>
2017-01-22 19:39:08 +0100

    Refactor Rewrite module

--- JavaRewrite/Rewrite.hs
+++ JavaRewrite/Rewrite.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE DeriveFunctor, FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, GeneralizedNewtypeDeriving #-}
 module JavaRewrite.Rewrite where
 
 import Control.Monad
+import Data.Monoid (Any(..))
 
 import Language.Java.Syntax
 
@@ -10,54 +11,44 @@ import JavaRewrite.Match
 import JavaRewrite.ApplySubst
 import JavaRewrite.Traversals
 
-newtype Apply a = Apply (Bool, a) deriving (Eq, Show, Functor)
+newtype RewriteResult a = RewriteResult (Any, a) deriving (Eq, Show, Functor, Applicative, Monad)
 
-instance Applicative Apply where
-  pure = return
-  (<*>) = ap
+rewriteSuccess :: a -> RewriteResult a
+rewriteSuccess exp = RewriteResult (Any True, exp)
 
-instance Monad Apply where
-  return x = Apply (False, x)
-  Apply (s1, e1) >>= k
-    | Apply (s2, e2) <- k e1
-       = Apply (s1 || s2, e2)
+getResult :: RewriteResult a -> a
+getResult (RewriteResult (_, a)) = a
 
-applySuccess :: a -> Apply a
-applySuccess exp = Apply (True, exp)
+isSuccess :: RewriteResult a -> Bool
+isSuccess (RewriteResult (Any b, _)) = b
 
-getResult :: Apply a -> a
-getResult (Apply (_, a)) = a
-
-isSuccess :: Apply a -> Bool
-isSuccess (Apply (b, _)) = b
-
--- | Apply a rule immediately to the expression.
+-- | RewriteResult a rule immediately to the expression.
 -- Returns @Just new_expr@ if it succeeded, @Nothing@ otherwise.
-applyRule :: Rule -> Exp -> Apply Exp
+applyRule :: Rule -> Exp -> RewriteResult Exp
 applyRule (Rule pattern rhs) exp =
     case match pattern exp of
-      Just subst -> applySuccess (applySubst subst rhs)
+      Just subst -> rewriteSuccess (applySubst subst rhs)
       Nothing    -> return exp
 
--- | Apply a set of rules to an expression in turn. Succeeds if any of the rules succeeded.
-applyRules :: [Rule] -> Exp -> Apply Exp
+-- | RewriteResult a set of rules to an expression in turn. Succeeds if any of the rules succeeded.
+applyRules :: [Rule] -> Exp -> RewriteResult Exp
 applyRules = foldr (>=>) return . map applyRule
 
 -- | Repeat a transformation until it fails.
 -- Succeeds if the transformation succeeded at least once.
-repeatUntilFailure :: (a -> Apply a) -> a -> Apply a
+repeatUntilFailure :: (a -> RewriteResult a) -> a -> RewriteResult a
 repeatUntilFailure transform x =
   let result = transform x
   in if isSuccess result
-     then repeatUntilFailure transform (getResult result) >>= applySuccess
+     then repeatUntilFailure transform (getResult result) >>= rewriteSuccess
      else result
 
--- | Apply rules to each subexpression in a top-down manner.
-applyRulesTopdown :: [Rule] -> Exp -> Apply Exp
+-- | RewriteResult rules to each subexpression in a top-down manner.
+applyRulesTopdown :: [Rule] -> Exp -> RewriteResult Exp
 applyRulesTopdown = repeatUntilFailure . topdown subexpressions . repeatUntilFailure . applyRules
 
--- | Apply a set of rules to a value containing expressions.
-rewrite :: Substructure Exp a => [Rule] -> a -> Apply a
+-- | RewriteResult a set of rules to a value containing expressions.
+rewrite :: Substructure Exp a => [Rule] -> a -> RewriteResult a
 rewrite rules = expressions (applyRulesTopdown rules)
 
 rewriteCompilationUnit :: [Rule] -> CompilationUnit -> CompilationUnit
