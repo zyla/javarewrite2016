23020ecaa6cfe86d743957f732fce78d14979c73
Maciej Bielecki <zyla@prati.pl>
2019-07-04 17:37:42 +0200

    Generalize Substructure to work for arbitrary types, not just Exp

--- JavaRewrite/Traversals.hs
+++ JavaRewrite/Traversals.hs
@@ -1,6 +1,6 @@
 -- | Generic traversals for Java AST.
 {-# LANGUAGE DefaultSignatures, FlexibleContexts, FlexibleInstances, TypeOperators,
-    DeriveGeneric, MultiParamTypeClasses, UndecidableInstances #-}
+    DeriveGeneric, MultiParamTypeClasses, UndecidableInstances, ConstraintKinds #-}
 module JavaRewrite.Traversals where
 
 import Control.Monad
@@ -12,43 +12,45 @@ class Substructure s a where
   -- | Traverse the values of type 's' in 'a'.
   substructure :: Applicative f => (s -> f s) -> a -> f a
 
-  default substructure :: (Generic a, GSubstructure s (Rep a), Applicative f) => (s -> f s) -> a -> f a
+  default substructure :: (GenericSubstructure s a, Applicative f) => (s -> f s) -> a -> f a
   substructure inj = fmap to . gsubstruct inj . from
+
+type GenericSubstructure s a = (Generic a, GSubstructure s (Rep a))
 
 class GSubstructure s rep where
   gsubstruct :: Applicative f => (s -> f s) -> rep p -> f (rep p)
 
 -- | boilerplate to strip out metadata
-instance GSubstructure Exp a => GSubstructure Exp (M1 i c a) where
+instance GSubstructure s a => GSubstructure s (M1 i c a) where
   gsubstruct inj (M1 x) = M1 <$> gsubstruct inj x
 
-instance GSubstructure Exp U1 where
+instance GSubstructure s U1 where
   gsubstruct inj U1 = pure U1
 
-instance (GSubstructure Exp a, GSubstructure Exp b) => GSubstructure Exp (a :+: b) where
+instance (GSubstructure s a, GSubstructure s b) => GSubstructure s (a :+: b) where
   gsubstruct inj (L1 x) = L1 <$> gsubstruct inj x
   gsubstruct inj (R1 x) = R1 <$> gsubstruct inj x
 
-instance (GSubstructure Exp a, GSubstructure Exp b) => GSubstructure Exp (a :*: b) where
+instance (GSubstructure s a, GSubstructure s b) => GSubstructure s (a :*: b) where
   gsubstruct inj (a :*: b) = (:*:) <$> gsubstruct inj a <*> gsubstruct inj b
 
-instance Substructure Exp a => GSubstructure Exp (K1 i a) where
+instance Substructure s a => GSubstructure s (K1 i a) where
   gsubstruct inj (K1 x) = K1 <$> substructure inj x
 
--- Base case: Don't recurse further if Exp is found
-instance {-# OVERLAPPING #-} GSubstructure Exp (K1 i Exp) where
+-- Base case: Don't recurse further if s is found
+instance {-# OVERLAPPING #-} GSubstructure s (K1 i s) where
   gsubstruct inj (K1 x) = K1 <$> inj x
 
 -- Definitions for base types
-instance Substructure Exp Bool where substructure = emptyTraversal
-instance Substructure Exp Char where substructure = emptyTraversal
-instance Substructure Exp Int where substructure = emptyTraversal
-instance Substructure Exp Double where substructure = emptyTraversal
-instance Substructure Exp Integer where substructure = emptyTraversal
+instance Substructure s Bool where substructure = emptyTraversal
+instance Substructure s Char where substructure = emptyTraversal
+instance Substructure s Int where substructure = emptyTraversal
+instance Substructure s Double where substructure = emptyTraversal
+instance Substructure s Integer where substructure = emptyTraversal
 
-instance GSubstructure Exp (K1 R a) => Substructure Exp (Maybe a)
-instance GSubstructure Exp (K1 R a) => Substructure Exp [a]
-instance (Substructure Exp a, Substructure Exp b) => Substructure Exp (a, b)
+instance GSubstructure s (K1 R a) => Substructure s (Maybe a)
+instance GSubstructure s (K1 R a) => Substructure s [a]
+instance (Substructure s a, Substructure s b) => Substructure s (a, b)
 
 emptyTraversal :: Applicative f => (a -> f a) -> b -> f b
 emptyTraversal _ = pure
@@ -56,57 +58,58 @@ emptyTraversal _ = pure
 --------------------------------------------------------------------------------
 -- BEGIN MOST BEAUTIFUL CODE IN THIS PROJECT
 --------------------------------------------------------------------------------
-instance Substructure Exp Annotation
-instance Substructure Exp ArrayIndex
-instance Substructure Exp ArrayInit
-instance Substructure Exp AssignOp
-instance Substructure Exp Block
-instance Substructure Exp BlockStmt
-instance Substructure Exp Catch
-instance Substructure Exp ClassBody
-instance Substructure Exp ClassDecl
-instance Substructure Exp ClassType
-instance Substructure Exp CompilationUnit
-instance Substructure Exp ConstructorBody
-instance Substructure Exp Decl
-instance Substructure Exp Diamond
-instance Substructure Exp ElementValue
-instance Substructure Exp EnumBody
-instance Substructure Exp EnumConstant
-instance Substructure Exp ExplConstrInv
-instance Substructure Exp FieldAccess
-instance Substructure Exp ForInit
-instance Substructure Exp FormalParam
-instance Substructure Exp Ident
-instance Substructure Exp ImportDecl
-instance Substructure Exp InterfaceBody
-instance Substructure Exp InterfaceDecl
-instance Substructure Exp InterfaceKind
-instance Substructure Exp LambdaExpression
-instance Substructure Exp LambdaParams
-instance Substructure Exp Lhs
-instance Substructure Exp Literal
-instance Substructure Exp MemberDecl
-instance Substructure Exp MethodBody
-instance Substructure Exp MethodInvocation
-instance Substructure Exp Modifier
-instance Substructure Exp Name
-instance Substructure Exp Op
-instance Substructure Exp PackageDecl
-instance Substructure Exp PrimType
-instance Substructure Exp RefType
-instance Substructure Exp Stmt
-instance Substructure Exp SwitchBlock
-instance Substructure Exp SwitchLabel
-instance Substructure Exp Type
-instance Substructure Exp TypeArgument
-instance Substructure Exp TypeDecl
-instance Substructure Exp TypeDeclSpecifier
-instance Substructure Exp TypeParam
-instance Substructure Exp VarDecl
-instance Substructure Exp VarDeclId
-instance Substructure Exp VarInit
-instance Substructure Exp WildcardBound
+instance GSubstructure s (Rep Annotation)        => Substructure s Annotation
+instance GSubstructure s (Rep ArrayIndex)        => Substructure s ArrayIndex
+instance GSubstructure s (Rep ArrayInit)         => Substructure s ArrayInit
+instance GSubstructure s (Rep AssignOp)          => Substructure s AssignOp
+instance GSubstructure s (Rep BlockStmt)         => Substructure s BlockStmt
+instance GSubstructure s (Rep Block)             => Substructure s Block
+instance GSubstructure s (Rep Catch)             => Substructure s Catch
+instance GSubstructure s (Rep ClassBody)         => Substructure s ClassBody
+instance GSubstructure s (Rep ClassDecl)         => Substructure s ClassDecl
+instance GSubstructure s (Rep ClassType)         => Substructure s ClassType
+instance GSubstructure s (Rep CompilationUnit)   => Substructure s CompilationUnit
+instance GSubstructure s (Rep ConstructorBody)   => Substructure s ConstructorBody
+instance GSubstructure s (Rep Decl)              => Substructure s Decl
+instance GSubstructure s (Rep Diamond)           => Substructure s Diamond
+instance GSubstructure s (Rep ElementValue)      => Substructure s ElementValue
+instance GSubstructure s (Rep EnumBody)          => Substructure s EnumBody
+instance GSubstructure s (Rep EnumConstant)      => Substructure s EnumConstant
+instance GSubstructure s (Rep ExplConstrInv)     => Substructure s ExplConstrInv
+instance GSubstructure s (Rep Exp)               => Substructure s Exp
+instance GSubstructure s (Rep FieldAccess)       => Substructure s FieldAccess
+instance GSubstructure s (Rep ForInit)           => Substructure s ForInit
+instance GSubstructure s (Rep FormalParam)       => Substructure s FormalParam
+instance GSubstructure s (Rep Ident)             => Substructure s Ident
+instance GSubstructure s (Rep ImportDecl)        => Substructure s ImportDecl
+instance GSubstructure s (Rep InterfaceBody)     => Substructure s InterfaceBody
+instance GSubstructure s (Rep InterfaceDecl)     => Substructure s InterfaceDecl
+instance GSubstructure s (Rep InterfaceKind)     => Substructure s InterfaceKind
+instance GSubstructure s (Rep LambdaExpression)  => Substructure s LambdaExpression
+instance GSubstructure s (Rep LambdaParams)      => Substructure s LambdaParams
+instance GSubstructure s (Rep Lhs)               => Substructure s Lhs
+instance GSubstructure s (Rep Literal)           => Substructure s Literal
+instance GSubstructure s (Rep MemberDecl)        => Substructure s MemberDecl
+instance GSubstructure s (Rep MethodBody)        => Substructure s MethodBody
+instance GSubstructure s (Rep MethodInvocation)  => Substructure s MethodInvocation
+instance GSubstructure s (Rep Modifier)          => Substructure s Modifier
+instance GSubstructure s (Rep Name)              => Substructure s Name
+instance GSubstructure s (Rep Op)                => Substructure s Op
+instance GSubstructure s (Rep PackageDecl)       => Substructure s PackageDecl
+instance GSubstructure s (Rep PrimType)          => Substructure s PrimType
+instance GSubstructure s (Rep RefType)           => Substructure s RefType
+instance GSubstructure s (Rep Stmt)              => Substructure s Stmt
+instance GSubstructure s (Rep SwitchBlock)       => Substructure s SwitchBlock
+instance GSubstructure s (Rep SwitchLabel)       => Substructure s SwitchLabel
+instance GSubstructure s (Rep TypeArgument)      => Substructure s TypeArgument
+instance GSubstructure s (Rep TypeDeclSpecifier) => Substructure s TypeDeclSpecifier
+instance GSubstructure s (Rep TypeDecl)          => Substructure s TypeDecl
+instance GSubstructure s (Rep TypeParam)         => Substructure s TypeParam
+instance GSubstructure s (Rep Type)              => Substructure s Type
+instance GSubstructure s (Rep VarDeclId)         => Substructure s VarDeclId
+instance GSubstructure s (Rep VarDecl)           => Substructure s VarDecl
+instance GSubstructure s (Rep VarInit)           => Substructure s VarInit
+instance GSubstructure s (Rep WildcardBound)     => Substructure s WildcardBound
 --------------------------------------------------------------------------------
 -- END MOST BEAUTIFUL CODE IN THIS PROJECT
 --------------------------------------------------------------------------------
